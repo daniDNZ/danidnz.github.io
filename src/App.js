50efd64b8078b1d9999c8564220522388b7ff505
danisanz <danielsanzelguer@gmail.com>
2022-05-16 18:23:57 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Navigation from './components/Navigation';
+import { Container, Stack } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App fw-lighter">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navigation />
       </header>
+      <main className='text-light mt-5 bg-dark'>
+        <section className='vh-100'>
+          <Container>
+            <Stack gap={3}>
+              <h1 className='display-3 text-center'>Welcome to my page</h1>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus inventore fuga voluptas sint similique qui architecto est? Fugit earum, velit praesentium eum cum totam neque magnam! Doloremque tenetur similique aliquid?</p>
+            </Stack>
+          </Container>
+        </section>
+        <section className='vh-100 bg-light text-dark'>
+          <Container>
+            <Stack gap={3}>
+              <h1 className='display-3 text-center'>Welcome to my page</h1>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus inventore fuga voluptas sint similique qui architecto est? Fugit earum, velit praesentium eum cum totam neque magnam! Doloremque tenetur similique aliquid?</p>
+            </Stack>
+          </Container>
+        </section>
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { Navbar, Container, Nav, NavDropdown, Offcanvas, Stack } from "react-bootstrap";
+
+export default function Navigation() {
+    const [show, setShow] = useState(false);
+
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+
+    return (
+        <>
+            <Navbar key="lg" bg="dark" variant="dark" expand="lg" fixed="top">
+                <Container>
+                    <Navbar.Brand href="/">dani.sanz</Navbar.Brand>
+                    <Navbar.Toggle onClick={handleShow} />
+                    <Navbar.Offcanvas show={show} onHide={handleClose} id="navbar-nav" aria-labelledby="navbar-nav-label" placement="end">
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id="navar-nav-label">
+                                Menú
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body className="justify-content-end">
+                        <Nav activeKey="1" onSelect={handleClose}>
+                            <Nav.Link href="/" eventKey="1">Home</Nav.Link>
+                            <NavDropdown title="Proyectos" id="projects-nav-dropdown" menuVariant="dark" >
+                                <NavDropdown.Item href="/pages_example" eventKey="2.1" className="fw-lighter">Pages Example</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/2.2" eventKey="2.2" className="fw-lighter">Another example</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
+            
+        </>
+    )
+}
