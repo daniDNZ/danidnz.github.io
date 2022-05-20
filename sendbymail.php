6ba889da256c34144a01d563d88f9064f7b831af
danisanz <danielsanzelguer@gmail.com>
2022-05-20 23:11:55 +0200

    Updates

--- sendbymail.php
+++ sendbymail.php
@@ -41,8 +41,7 @@
 
         return json_encode([
             'sent' => $sent,
-            'message' => $message,
-            'email' => $email_from
+            'message' => $message
         ]);
     }
 
