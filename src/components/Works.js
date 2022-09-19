897fb924102952fa7837278379335220b302ef89
danisanz <daniDNZ@users.noreply.github.com>
2022-09-19 19:27:36 +0200

    + works

--- src/components/Works.js
+++ src/components/Works.js
@@ -6,10 +6,34 @@ export default function Works() {
     return (
         <>
             <section id="works" className="section section--works">
-                
+
                 <h2 className="h2 works__h2">Last works</h2>
                 <article className="article">
-                    {/* <img src={w3} alt="Imagen" className="img article__img" loading="lazy" /> */}
+                    <h3 className="h3 article__h3">Web hotel Miranda (versión smartphone)</h3>
+                    <ul className="article__ul">
+                        <li className="article__li">HTML5</li>
+                        <li className="article__li">SASS</li>
+                        <li className="article__li">JavaScript</li>
+                    </ul>
+                    <ul className="article__ul-social">
+                        <li className="article__li-social"><a href="https://danidnz.github.io/web-miranda/" className="a article__link" title="Web Hotel Miranda" target="_blank" rel="noreferrer noopener">Demo <RiExternalLinkLine /></a></li>
+                        <li className="article__li-social"><a href="https://github.com/daniDNZ/web-miranda" className="a article__link" title="Repositorio" target="_blank" rel="noreferrer noopener">GitHub <RiExternalLinkLine /></a></li>
+                    </ul>
+                </article>
+                <article className="article">
+                    <h3 className="h3 article__h3">Hotel Dashboard</h3>
+                    <ul className="article__ul">
+                        <li className="article__li">REACTJS</li>
+                        <li className="article__li">REDUX</li>
+                        <li className="article__li">JS / TS</li>
+                        <li className="article__li">StyledComponents</li>
+                    </ul>
+                    <ul className="article__ul-social">
+                        <li className="article__li-social"><a href="https://danidnz.github.io/hotel-admin-dashboard/" className="a article__link" title="Hotel Dashboard" target="_blank" rel="noreferrer noopener">Demo <RiExternalLinkLine /></a></li>
+                        <li className="article__li-social"><a href="https://github.com/daniDNZ/hotel-admin-dashboard" className="a article__link" title="Repositorio" target="_blank" rel="noreferrer noopener">GitHub <RiExternalLinkLine /></a></li>
+                    </ul>
+                </article>
+                <article className="article">
                     <h3 className="h3 article__h3">Twitter Mirroring (Versión web smartphones y tablets)</h3>
                     <ul className="article__ul">
                         <li className="article__li">JavaScript</li>
